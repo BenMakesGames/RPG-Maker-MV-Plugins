a91d529672f85c4fdd4e8f45805bee0d1d649a1d
Ben H <ben@benmakesgames.com>
2018-10-28 09:56:58 -0400

    a tiny plugin

---
+++ bentelk_NoSpecialBattleMusic.js
@@ -0,0 +1,14 @@
+/*:
+ * @plugindesc When entering battle, the music is not changed; the map music continues.
+ * @author Ben Hendel-Doying
+ *
+ * @help That's it!
+ */
+
+BattleManager.playBattleBgm = function() {
+    // do nothing!
+};
+
+Scene_Map.prototype.stopAudioOnBattleStart = function() {
+    // do nothing!
+};
