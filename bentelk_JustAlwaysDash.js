bd7739376c04b23f9f535fdb60c4b8c13bf3e1f7
Ben H <ben@benmakesgames.com>
2020-07-05 22:56:04 -0400

    added more functions to JustAlwaysDash

--- bentelk_JustAlwaysDash.js
+++ bentelk_JustAlwaysDash.js
@@ -2,18 +2,42 @@
  * @plugindesc The player always moves at dashing speed, because why wouldn't you?
  * @author Ben Hendel-Doying
  *
- * @help
+ * @help Disabling dashing via map settings still works. Additionally, this plugin allows
+ * you to disable/enable dashing via Script instructions. To disable:
+ *
+ *   $gamePlayer.disableDashing();
+ *
+ * Dashing will remain disabled until enabled again:
+ *
+ *   $gamePlayer.enableDashing();
+ *
+ * (enableDashing does NOT override map settings.)
+ *
  * The "always dash" option is removed from the options menu.
  *
  * It probably goes without saying, but this plugin is NOT compatible with
  * bentelk_DashyBoots.
+ *
+ * Compatibility:
+ * * REPLACES Game_Player updateDashing
+ * * REPLACES Window_Options addGeneralOptions
+ * * ADDS Game_Player enableDashing
+ * * ADDS Game_Player disableDashing
  */
+
+Game_Player.prototype.enableDashing = function() {
+    this._dashingDisabled = false;
+};
+
+Game_Player.prototype.disableDashing = function() {
+    this._dashingDisabled = true;
+};
 
 Game_Player.prototype.updateDashing = function() {
     if (this.isMoving()) {
         return;
     }
-    this._dashing =  this.canMove() && !this.isInVehicle() && !$gameMap.isDashDisabled();
+    this._dashing = !this._dashingDisabled && this.canMove() && !this.isInVehicle() && !$gameMap.isDashDisabled();
 };
 
 Window_Options.prototype.addGeneralOptions = function() {
